bfca76fb6fd2dc4e09b9ede44b6c6800faca8ef3
ballway <ballway.w@gmail.com>
2023-11-10 21:21:23 +0800

    BookProject.WebAPI 新增 API

--- BookProject/BookProject.WebAPI/Controllers/BookController.cs
+++ BookProject/BookProject.WebAPI/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using BookProject.Contract.DTO;
 using BookProject.Contract.Service;
+using BookProject.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookProject.WebAPI.Controllers
@@ -24,7 +25,7 @@ namespace BookProject.WebAPI.Controllers
         /// <summary>
         /// 取得所有書籍資料。
         /// </summary>
-        [HttpGet("/book/getall")]
+        [HttpGet("/book/all")]
         public IActionResult GetAllBooks()
         {
             List<BookInfo> bookInfos = _bookService.GetAllBooks();
@@ -35,7 +36,7 @@ namespace BookProject.WebAPI.Controllers
         /// <summary>
         /// 取得指定 BookId 的書籍資料。
         /// </summary>
-        [HttpGet("/book/get/{bookid}")]
+        [HttpGet("/book/{bookid}")]
         public IActionResult Get(string bookid)
         {
             BookInfo bookInfo = _bookService.GetBookById(bookid);
@@ -43,5 +44,90 @@ namespace BookProject.WebAPI.Controllers
             return Ok(bookInfo);
         }
 
+        /// <summary>
+        /// 新增一筆書籍資料。
+        /// </summary>
+        [HttpPost("/book")]
+        public IActionResult Post([FromBody] NewBook newBook)
+        {
+            BookInfo bookInfo = new BookInfo();
+            bookInfo.BookId = GetGUID();
+            bookInfo.DisplayName = newBook.DisplayName;
+            bookInfo.LastModifiedDateTime = DateTime.UtcNow;
+            _bookService.CreateBook(bookInfo);
+            bookInfo = _bookService.GetBookById(bookInfo.BookId);
+
+            return Ok(bookInfo);
+        }
+
+        /// <summary>
+        /// 更新一筆書籍資料 (修改名稱)。
+        /// </summary>
+        [HttpPut("/book")]
+        public IActionResult Put([FromBody] UpdateBook updateBook)
+        {
+            BookInfo bookInfo = new BookInfo();
+            bookInfo.BookId = updateBook.BookId;
+            bookInfo.DisplayName = updateBook.DisplayName;
+            bookInfo.LastModifiedDateTime = DateTime.UtcNow;
+            _bookService.UpdateBook(bookInfo);
+            bookInfo = _bookService.GetBookById(bookInfo.BookId);
+
+            return Ok(bookInfo);
+        }
+
+        /// <summary>
+        /// 刪除一筆書籍資料。
+        /// </summary>
+        [HttpDelete("/book/{bookid}")]
+        public IActionResult Delete(string bookid)
+        {
+            _bookService.DeleteBookById(bookid);
+
+            return Ok("Delete book successfully.");
+        }
+
+
+        /// <summary>
+        /// 建立一本新書籍的參數。
+        /// </summary>
+        public class NewBook
+        {
+            /// <summary>
+            /// 書籍顯示名稱。
+            /// </summary>
+            public string DisplayName { get; set; }
+
+            /// <summary>
+            /// 書籍作者 Id。
+            /// </summary>
+            public string AuthorId { get; set; }
+
+            /// <summary>
+            /// 書籍類別 Id。
+            /// </summary>
+            public string CategoryId { get; set; }
+        }
+
+        /// <summary>
+        /// 更新一本舊書籍的參數。
+        /// </summary>
+        public class UpdateBook
+        {
+            /// <summary>
+            /// 書籍 Id。
+            /// </summary>
+            public string BookId { get; set; }
+
+            /// <summary>
+            /// 書籍顯示名稱。
+            /// </summary>
+            public string DisplayName { get; set; }
+        }
+
+        private string GetGUID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
     }
 }
